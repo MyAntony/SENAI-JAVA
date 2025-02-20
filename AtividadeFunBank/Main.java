0048d118c5f17686d2e5accd8eef332b8f0049d7
Antony Rafael <antony05rafael@gmail.com>
2025-02-20 14:27:59 -0300

    Testando alteração de nome

--- AtividadeFunBank/Main.java
+++ AtividadeFunBank/Main.java
@@ -8,7 +8,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Funcionario funcionario = new Funcionario ("001", "Antony", "Rua Onze de Maio, 10, Ribeira", "+55 71 98952-0146", "antony05rafael@gmail.com", new ContaBancaria("323 Mercado Pago", "0001", "2755493880-7", TipoDeConta.CORRENTE, 22.20, 1.600,00));
+        Funcionario funcionario = new Funcionario ("001", "Antony", "Rua Onze de Maio, 10, Ribeira", "+55 71 98952-0146", "antony05rafael@gmail.com", new ContaBancaria("323 Mercado Pago", "0001", "2755493880-7", TipoDeConta.CORRENTE, 22.20, 1.600));
         System.out.println(funcionario.toString());
     }
 }
