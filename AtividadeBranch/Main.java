46585431c997495a03257b1384ea385a4f8aa753
Antony Rafael <antony05rafael@gmail.com>
2025-02-20 21:26:40 -0300

    Novos aprendizados

--- AtividadeBranch/Main.java
+++ AtividadeBranch/Main.java
@@ -1,5 +1,9 @@
 package AtividadeBranch;
 
-public class Main {
-
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Antony Rafael dos Santos Rufino");
+    }
 }
